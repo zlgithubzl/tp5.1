742554acb622814eca7b5359a65460d539ee6d1e
long_zhao <596526704@qq.com>
2020-06-29 18:00:55 +0800

    修改

--- application/common/command/reUpdateDbData.php
+++ application/common/command/reUpdateDbData.php
@@ -14,31 +14,20 @@
 	}
 	protected function execute($input, $output)
 	{  $return = $this->getKey(); 
-	    $output->writeln('输出了：'.$return);
+	    //$output->writeln('输出了：'.$return);
 	}
 	private function getKey()
 	{
-	   $redis = new \Redis();
-	   $redis->connect('127.0.0.1',6381);
-	   $userId = 1388;
-	   $signStartTime = '2020-01-01';
-	   $cacheKey = 'sign_'.$userId;
-	   $offset = intval((time()-strtotime($signStartTime))/86400);
-	    //$redis->setBit($cacheKey,$offset,1);
-	   // $bitStatus = $redis->getBit($cacheKey,$offset);
-	    //$signCount = $redis->bitCount($cacheKey);
-	    //return $signCount;
-	   //$key_value = $redis->get('name');
-	   ///return $bitStatus;
-
-
-	   //die;
-	  // include_once  __DIR__.'/../../../my_vendor/DaemonCommand.php';
-	$daemon=new \DaemonCommand(true);
-	$daemon->daemonize();
-	$daemon->start(2);//开启2个子进程工作
-	echo '进程id:'.posix_getpid().";父进程：".posix_getppid().'\r\n';
-	sleep(600);	
+	   //$redis = new \Redis();
+	   //$redis->connect('127.0.0.1',6381);
+    	//$daemon=new \DaemonCommand(true);
+	//$daemon->daemonize();
+	//$daemon->start(2);//开启2个子进程工作
+	//echo '进程id:'.posix_getpid().";父进程：".posix_getppid().'\r\n';
+	//sleep(600);	
+	$m = new \mutiProcess();
+	$m->run(1);
+	//$m->wait(false);
 
 	}
 
--- extend/mutiProcess.php
+++ extend/mutiProcess.php
@@ -10,25 +10,30 @@
 	 *
 	 */
 	public function run($child_num){
+	    cli_set_process_title("aaa");
 	    $p_pname = cli_get_process_title();
+
 	    pcntl_signal_dispatch();
 	   $pid =  pcntl_fork();
-	   if($pid = -1){
+	   if($pid == -1){
 	     exit('fork failed');//此处可以优化为自定义异常
 	   }elseif($pid>0){
-
+	   die;echo 2;
+		exit(0);
+		$this->wait(false);
+		exit;
 	   }else{
 	    //子进程
-           echo "子进程已启动/r/n";
+           echo "子进程已启动\r\n";
 	    $s_pname = $p_pname."_s_".$child_num;
-	    cli_set_process_name($s_pname);//设置子进程名称
+	    cli_set_process_title($s_pname);//设置子进程名称
 	    if($this->signal_handlers){
             foreach($this->signal_handlers as $signal=>$callback){
                 pcntl_signal($signal,$callback);
             };
 	    }
 	    sleep(15);
-	    echo '子进程：'.$s_pname.'马上退出，bye'."/r/n";
+	    echo '子进程：'.$s_pname.'马上退出，bye'."\r\n";
 	    
 	   }
 
